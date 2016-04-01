4b27c74dd45f19a567aeec9715f4f16fa0da5015
Jake Long <jakeprogrammer@gmail.com>
2016-04-01 01:07:50 -0500

    Images endpoint now saves the base64 image to disc

--- images.js
+++ images.js
@@ -1,9 +1,30 @@
+var gm = require("gm");
+var fs = require("fs");
+
 module.exports = function(app, sessions)
 {
 	app.post("/images", function(req, res)
 	{
-		console.log(req.body);
-		console.log(req.test);
-		res.send("OK");
+		var sessionID = req.body.sessionID;
+	
+		var imageNumber = sessions.search(sessionID); //Used as the name of the file
+		sessions.set(sessionID, imageNumber + 1);
+
+		//Save the image to disk
+		var base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
+		fs.writeFile(sessionID + "/" + imageNumber + ".png", base64Data, 'base64', function(err)
+		{
+			console.log(err);
+		});
+
+		/*var cropCoordinates = req.body.crop;
+
+		//If the user wants the photo cropped
+		if(cropCoordinates.x1 != -1)
+		{
+
+		}*/
+
+		res.send(JSON.stringify({"status":201}));
 	});
 }

452ff5ff0c70767ee5a2e7c23a743a183e117eaa
Rodney <rodn725@gmail.com>
2016-03-31 23:32:08 -0500

    Reworked organization and GET

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var gm = require("gm");
 var HashMap = require("hashmap");
 var sessions = new HashMap();
@@ -8,4 +9,17 @@ var imgs = require("./images")(app, sessions);
 var collage = require("./collage.js")(app, sessions, gm);
 
 app.listen(3000);
+app.use(express.static(__dirname + '/HTML'));
+app.use('/css', express.static('css'));
+app.use('/images', express.static('images'));
+app.use('/js', express.static('js'));
+//Store all HTML files in view folder.
+//app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+
 console.log("Application now listening on port 3000");

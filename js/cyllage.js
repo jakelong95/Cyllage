cbc4b13d06179748777d942a0373edafdc3843f1
awolter <awolter333@gmail.com>
2016-03-31 19:36:05 -0500

    Added tabs that switch between My Images and Layers

--- js/cyllage.js
+++ js/cyllage.js
@@ -1,3 +1,19 @@
-/**
- * Created by Adam on 3/31/16.
- */
+/* Global Variables */
+
+myImagesButton_jQ = $("#myImagesButton");
+layersButton_jQ = $("#layersButton");
+
+myImagesTab_jQ = $("#myImagesTab");
+layersTab_jQ = $("#layersTab");
+
+
+$(document).ready(function() {
+    myImagesButton_jQ.click(function(){
+        myImagesTab_jQ.show();
+        layersTab_jQ.hide();
+    });
+    layersButton_jQ.click(function(){
+        layersTab_jQ.show();
+        myImagesTab_jQ.hide();
+    });
+});

5428b4372128b8bb39093c0ec30af1d58eb0e418
Jake Long <jakeprogrammer@gmail.com>
2016-04-01 01:53:39 -0500

    Put pseudo-code for drawing algorithm in a comment.

--- collage.js
+++ collage.js
@@ -3,5 +3,25 @@ module.exports = function(app, sessions)
 	app.get("/collage", function(req, res)
 	{
 	//TODO Delete the session from sessions, also delete the images and directory
+
+	/*
+	create an array of layers
+	var session = sessions.get(sessionID) //session id from query parameter
+	for each img in session.imgs
+		format will be:
+			{
+ 	        	file: the file for the image,
+   				layer: the layer the image is on,
+    			position: {x:int,y:int},
+    			rotate: int in degrees
+    		}
+    		if layer is > layers.size
+    			add a new layer
+
+    		draw the image on the layer at the position with the rotation
+    		//the layer needs to be saved to disc
+
+    draw each layer on each other in the specified order
+}	*/
 	});
 }

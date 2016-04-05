bbb701fb92d050284cce8905c3b2e0a3d60125ca
awolter <awolter333@gmail.com>
2016-04-05 02:25:37 -0500

    Added functionality for moving/resizing images on canvas
    - Added interact.js
    - Added hardcoded images for reference
    
    (Removed unneeded code)

--- js/cyllage.js
+++ js/cyllage.js
@@ -8,34 +8,7 @@ layersTab_jQ = $("#layersTab");
 
 
 
-// target elements with the "draggable" class
-/*
-interact('.draggable')
-    .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        restrict: {
-            restriction: "parent",
-            endOnly: true,
-            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-        // enable autoScroll
-        autoScroll: true,
-
-        // call this function on every dragmove event
-        onmove: dragMoveListener,
-        // call this function on every dragend event
-        onend: function (event) {
-            var textEl = event.target.querySelector('p');
-
-            textEl && (textEl.textContent =
-                'moved a distance of '
-                + (Math.sqrt(event.dx * event.dx +
-                    event.dy * event.dy)|0) + 'px');
-        }
-    });*/
-
+// Interact.js
 interact('.draggable')
     .draggable({
         onmove: window.dragMoveListener,

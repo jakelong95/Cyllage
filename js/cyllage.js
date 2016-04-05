136647ff991d5a8d2e313b1bb5ca715e3197188f
awolter <awolter333@gmail.com>
2016-04-05 02:23:36 -0500

    Added functionality for moving/resizing images on canvas
    - Added interact.js
    - Added hardcoded images for reference

--- js/cyllage.js
+++ js/cyllage.js
@@ -6,56 +6,90 @@ layersButton_jQ = $("#layersButton");
 imagesTab_jQ = $("#imagesTab");
 layersTab_jQ = $("#layersTab");
 
-var targ, offsetX, offsetY, coordX, coordY, drag;
 
-function startDrag(e) {
-    // determine event object
-    if (!e) {
-        var e = window.event;
-    }
-    if(e.preventDefault) e.preventDefault();
 
-    console.log(e);
+// target elements with the "draggable" class
+/*
+interact('.draggable')
+    .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        // enable autoScroll
+        autoScroll: true,
 
-    // IE uses srcElement, others use target
-    targ = e.target ? e.target : e.srcElement;
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+        onend: function (event) {
+            var textEl = event.target.querySelector('p');
 
-    if (targ.className != 'draggable') {return}
-    // calculate event X, Y coordinates
-    offsetX = e.clientX;
-    offsetY = e.clientY;
+            textEl && (textEl.textContent =
+                'moved a distance of '
+                + (Math.sqrt(event.dx * event.dx +
+                    event.dy * event.dy)|0) + 'px');
+        }
+    });*/
 
-    // assign default values for top and left properties
-    if(!targ.style.left) { targ.style.left='0px'}
-    if (!targ.style.top) { targ.style.top='0px'}
+interact('.draggable')
+    .draggable({
+        onmove: window.dragMoveListener,
+        restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        }
+    })
+    .resizable({
+        preserveAspectRatio: true,
+        edges: { left: true, right: true, bottom: true, top: true },
+        restrict: {
+            restriction: "parent"
+        }
+    })
+    .on('resizemove', function (event) {
+        var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-    // calculate integer values for top and left
-    // properties
-    coordX = parseInt(targ.style.left);
-    coordY = parseInt(targ.style.top);
-    drag = true;
+        // update the element's style
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
 
-    // move div element
-    document.onmousemove=dragDiv;
-    return false;
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
 
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    });
+
+function dragMoveListener (event) {
+    var target = event.target,
+    // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
 }
-function dragDiv(e) {
-    if (!drag) {return}
-    if (!e) { var e= window.event}
-    // var targ=e.target?e.target:e.srcElement;
-    // move div element
-    targ.style.left=coordX+e.clientX-offsetX+'px';
-    targ.style.top=coordY+e.clientY-offsetY+'px';
-    return false;
-}
-function stopDrag() {
-    drag=false;
-}
-window.onload = function() {
-    document.onmousedown = startDrag;
-    document.onmouseup = stopDrag;
-}
+
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
 
 
 
